b36b5ed6aeae965db08ca7a0c28d52815848607f
matin69 <matin3enator0@gmail.com>
2023-01-30 12:16:48 +0330

    config security for dev

--- app/src/main/java/com/mflix/app/security/SecurityConfigurer.java
+++ app/src/main/java/com/mflix/app/security/SecurityConfigurer.java
@@ -1,5 +1,6 @@
 package com.mflix.app.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,8 +13,19 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfigurer {
 
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        if (activeProfile.equals("production")) {
+            return configProductionSecurity(http);
+        } else {
+            return configDevSecurity(http);
+        }
+    }
+
+    private SecurityFilterChain configProductionSecurity(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers("/", "/signup").permitAll()
@@ -27,6 +39,10 @@ public class SecurityConfigurer {
                 )
                 .csrf().disable()
                 .cors().disable();
+        return http.build();
+    }
+
+    private SecurityFilterChain configDevSecurity(HttpSecurity http) throws Exception {
         return http.build();
     }
 
