f11f6bfa64a987361f6ca9c90421500d3b589863
matin69 <matin3enator0@gmail.com>
2023-02-05 12:56:25 +0330

    Create file distributor service module

---
+++ file-distributor/src/main/java/com/mflix/filedistributor/FileDistributorApplication.java
@@ -0,0 +1,13 @@
+package com.mflix.filedistributor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileDistributorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileDistributorApplication.class, args);
+    }
+
+}
---
+++ file-distributor/src/test/java/com/mflix/filedistributor/FileDistributorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mflix.filedistributor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileDistributorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
