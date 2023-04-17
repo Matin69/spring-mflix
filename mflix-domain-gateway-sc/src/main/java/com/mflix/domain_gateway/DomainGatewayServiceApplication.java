9be1d9f78a8c713a21503c94bb6b770da76d427b
mkanani <mkanani@payeshgaran.net>
2023-04-17 15:23:31 +0430

    Create domain gateway and composition microservices

---
+++ mflix-domain-gateway-sc/src/main/java/com/mflix/domain_gateway/DomainGatewayServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mflix.domain_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomainGatewayServiceApplication.class, args);
+    }
+}
---
+++ mflix-movie-composition-sc/src/main/java/com/mflix/movie_composition/MovieCompositionServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mflix.movie_composition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCompositionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieCompositionServiceApplication.class, args);
+    }
+}
