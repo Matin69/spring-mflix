2ecee38ddbf08c8e6a3846a944fea0d097230744
matin69 <matinknani69@gmail.com>
2023-05-02 10:53:51 +0330

    Create bons of admin gateway and service modules

---
+++ mflix-admin-gateway-sc/src/main/java/com/mflix/admin_gateway/AdminGatewayServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mflix.admin_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminGatewayServiceApplication.class, args);
+    }
+}
---
+++ mflix-admin-movie-sc/src/main/java/com/mflix/admin_movie/AdminMovieServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mflix.admin_movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminMovieServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminMovieServiceApplication.class, args);
+    }
+}
